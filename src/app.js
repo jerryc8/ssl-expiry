500025f78d91921b102a0cda395efb6d39a965a7
jerry <jerry123@jy8.net>
2023-02-24 16:15:21 -0800

    track unhealthy server count with Dict unhealthyCounts

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ import readline from "readline";
 // import { StatsD } from "node-statsd";
 import sslChecker from "ssl-checker";
 
-
 // from https://stackoverflow.com/a/59144918
 /**
  * @param {Date} date - the initial Date
@@ -35,12 +34,13 @@ const URGENT_EXPIRE_SOON_LIMIT_DAYS = 30;
 
 const checkServer = async (address) => {
   let sslPort = 443;
+  let service = 'Unknown';
   // TOIMPROVE: string comparison works only because of /24, use an IP address library instead
   if (address.startsWith('10.10.6.')) {
-    // Europa
+    service = 'Europa';
     sslPort = 4000;
   } else if (address.startsWith('10.10.8.')) {
-    // Callisto
+    service = 'Callisto';
     sslPort = 8000;
   }
   // TODO: connection issues - ensure that the team is notified.
@@ -49,38 +49,57 @@ const checkServer = async (address) => {
     result = await sslChecker(address, { method: "GET", port: sslPort });
   } catch (error) {
     await notifyBySlack('urgent', `Unable to connect to ${address}, error: ${error}`);
-    return;
+    return { service, healthy: false };
   }
 
   // Both the ssl-checker return value and new Date() are in UTC
   // Hence there is no need to account for timezone difference
 
-  // eg, validFrom: "2023-02-08T04:36:30.000Z"
+  // eg, validTo: "2023-05-03T04:36:29.000Z"
   const now = new Date();
+  const expiryDate = new Date(Date.parse(result.validTo));
+  const diffTime = expiryDate - now;
+  if (diffTime < 0) {
+    await notifyBySlack('extreme', `Certificate for server ${address} expired on: ${expiryDate.toISOString()}, please re-issue immediately`);
+    return { service, healthy: false };
+  }
+
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  if (diffDays > URGENT_EXPIRE_SOON_LIMIT_DAYS) {
+    await notifyBySlack('urgent', `Certificate for server ${address} will expire (< ${URGENT_EXPIRE_SOON_LIMIT_DAYS} days): ${expiryDate.toISOString()}`);
+    return { service, healthy: false };
+  }
+  // eg, validFrom: "2023-02-08T04:36:30.000Z"
   const issueDate = new Date(Date.parse(result.validFrom));
   const expectedIssueDate = getLastDayOccurence(now, EXPECTED_ISSUE_WEEKDAY);
   if (issueDate < expectedIssueDate) {
     await notifyBySlack('normal', `Certificate for server ${address} were not re-issued, issued: ${issueDate.toISOString()} (expected ${expectedIssueDate.toISOString()} or later)`);
   }
-
-  // eg, validTo: "2023-05-03T04:36:29.000Z"
-  const expiryDate = new Date(Date.parse(result.validTo));
-  const diffTime = expiryDate - now;
-  if (diffTime < 0) {
-    await notifyBySlack('extreme', `Certificate for server ${address} expired on: ${issueDate.toISOString()}, please re-issue immediately`);
-  }
-
-  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-  if (diffDays > URGENT_EXPIRE_SOON_LIMIT_DAYS) {
-    await notifyBySlack('urgent', `Certificate for server ${address} will expire (< 30 days): ${issueDate.toISOString()}`);
-  }
+  // still considered healthy even if recent issuance did not happen
+  return { service, healthy: true };
 };
 
+// TOIMPROVE - if file is large, use `line-by-line` package to avoid
+// loading the entire file into memory
 const SERVER_ADDRESSES = 'ip_addresses.txt';
 const readInterface = readline.createInterface({
   input: fs.createReadStream(SERVER_ADDRESSES)
 });
 // TOIMPROVE - possible to batch parallelize the checks
+const unhealthyCounts = {};
 for await (const line of readInterface) {
-  await checkServer(line);
+  const {service, healthy} = await checkServer(line);
+  if (!healthy) {
+    const count = unhealthyCounts[service];
+    if (Number.isInteger(count)) {
+      unhealthyCounts[service] += 1;
+    } else {
+      unhealthyCounts[service] = 1;
+    }
+  }
+}
+// record how many unhealthy instances exist for each service
+for (const service of Object.keys(unhealthyCounts)) {
+  const c = unhealthyCounts[service];
+  console.log(`service ${service}: ${c} unhealthy`);
 }
